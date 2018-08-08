fb1e7252e99818bb5ee2d8e1b2ffb6f8b4748d45
Charles ⚡ <charlespeters42@gmail.com>
2018-08-08 13:36:47 -0700

    adding more explicit naming

--- index.js
+++ index.js
@@ -1,68 +1,60 @@
-class Anchorage {
-  constructor (cn, options = {}) {
+export default class Anchorage {
+  constructor(baseSelector, options = {}) {
     // Defaults
     const dft = {
-      linkClass: 'c-anchorage',
-      headlines: 'h1, h2, h3, h4, h5, h6'
-    }
+      linkClass: "Anchorage",
+      headlines: "h1, h2, h3, h4, h5, h6"
+    };
 
-    this.options = {
-      linkClass: options.linkClass ? options.linkClass : dft.linkClass,
-      headlines: options.headlines ? options.headlines : dft.headlines
-    }
+    this.options = Object.assign({}, dft, options);
 
-    const headlines = this.headlines(cn)
+    const headlines = this.headlines(baseSelector);
 
     for (let i = 0; i < headlines.length; i++) {
-      const txt = this.parser(headlines[i])
-      const count = i + 1
-      const ref = `${txt}-${count}`
+      const txt = this.parser(headlines[i]);
+      const count = i + 1;
+      const ref = `${txt}-${count}`;
 
-      this.setter(headlines[i], ref)
-      this.link(headlines[i], this.element(headlines[i].getAttribute('id')))
+      this.setter(headlines[i], ref);
+      this.link(headlines[i], this.element(headlines[i].getAttribute("id")));
     }
-
-    console.log(options)
   }
 
   // Collects All the Headlines
-  headlines (x) {
-    const cln = document.querySelector(x)
-    const xh = cln.querySelectorAll(this.options.headlines)
+  headlines(x) {
+    const headlines = document.querySelector(x);
 
-    return xh
+    return headlines.querySelectorAll(this.options.headlines);
   }
 
   // Creates the ID/reference
-  parser (x) {
-    let s = x.innerHTML
-    return s.toLowerCase().replace(' ', '-')
+  parser(x) {
+    let s = x.innerHTML;
+    return s.toLowerCase().replace(" ", "-");
   }
 
   // Sets ID attribute
-  setter (x, y) {
-    if (!x.hasAttribute('id')) {
-      x.setAttribute('id', y)
+  setter(x, y) {
+    if (!x.hasAttribute("id")) {
+      x.setAttribute("id", y);
     }
   }
 
   // Creates Link Element
-  element (y) {
-    const ref = `#${y}`
-    const elm = document.createElement('a')
-    const t = document.createTextNode('Link')
+  element(y) {
+    const ref = `#${y}`;
+    const elm = document.createElement("a");
+    const t = document.createTextNode("Link");
 
-    elm.appendChild(t)
-    elm.setAttribute('href', ref)
-    elm.setAttribute('class', this.options.linkClass)
+    elm.appendChild(t);
+    elm.setAttribute("href", ref);
+    elm.setAttribute("class", this.options.linkClass);
 
-    return elm
+    return elm;
   }
 
   // Appends Link Element
-  link (x, y) {
-    return x.appendChild(y)
+  link(x, y) {
+    return x.appendChild(y);
   }
 }
-
-export default Anchorage
